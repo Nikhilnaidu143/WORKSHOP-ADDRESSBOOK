b0537ec56e6ba7f6b9240ad93fe6f8ab4e7bb8d8
saitarun135 <nikhilnaidu473@gmail.com>
2022-01-10 03:14:55 +0530

    UC-1:- Added contact details.

---
+++ src/com/workshop/addressbook/entity/Contact.java
@@ -0,0 +1,15 @@
+package com.workshop.addressbook.entity;
+
+public class Contact {
+
+	/** Instance variables. **/
+	String first_name;
+	String last_name;
+	String address;
+	String city;
+	String state;
+	int zip_code;
+	long phone_number;
+	String email;
+
+}
--- src/com/workshop/addressbook/main/AddressBookMain.java
+++ src/com/workshop/addressbook/main/AddressBookMain.java
@@ -3,6 +3,7 @@ package com.workshop.addressbook.main;
 public class AddressBookMain {
 
 	public static void main(String[] args) {
-		System.out.println("--------WELCOME TO ADDRESS BOOK PROBLEM--------");
+		/** Displayed welcome message. **/
+		System.out.println("--------WELCOME TO ADDRESS BOOK SYSTEM--------");
 	}
 }
