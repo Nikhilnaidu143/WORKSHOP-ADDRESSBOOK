949ce6bb7de0300c38cf9417df07ead61b1d1f9c
saitarun135 <nikhilnaidu473@gmail.com>
2022-01-10 03:11:37 +0530

    START:- Displayed welcome message.

---
+++ src/com/workshop/addressbook/main/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.workshop.addressbook.main;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("--------WELCOME TO ADDRESS BOOK PROBLEM--------");
+	}
+}
